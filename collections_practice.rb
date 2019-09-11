0be154e8724ffba4bf29c9413ef8c719f8a0a53d
Oscar Shoudt <shoudto@gmail.com>
2019-09-11 16:09:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,57 +1,57 @@
 require 'pry'
 
-# Question 1 
+# Question 1
 def sort_array_asc(array)
   ascending =+ 1
   array.sort_by { |num| num * ascending }
 
-end 
+end
 
-# Question 2 
+# Question 2
 def sort_array_desc(array)
   descending = -1
   array.sort_by { |num| num * descending }
-end 
+end
 
-# Question 3 
+# Question 3
 def sort_array_char_count(array)
   array.sort_by { |word| word.length}
-end 
+end
 
 # Question 4
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
-  array 
-end 
+  array
+end
 
 # Question 5
-def reverse_array(array) 
-  array.reverse 
-end 
+def reverse_array(array)
+  array.reverse
+end
 
-# Question 6 
+# Question 6
 def kesha_maker(array)
-  #binding.pry 
+  #binding.pry
   array.each do |word|
     word[2] = "$"
-  end 
+  end
   array
-end 
+end
 
-# Question 7 
+# Question 7
 def find_a(array)
   array.select { |word| word.start_with?('a') }
-end 
+end
 
-# Question 8 
+# Question 8
 def sum_array(array)
-  array.sum 
-end 
+  array.sum
+end
 
-# Question 9 
+# Question 9
 def add_s(array)
  # binding.pry
   array.each_with_index.map do |word, index|
     index == 1 ? word : word + "s"
-  end 
-end 
+  end
+end
